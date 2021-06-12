0d55c4a8cd75b9ec87ca9a9a88ff71fd93d452dc
Finotelli, Carlos Eduardo Pereira <carlos.finotelli@outlook.com>
2021-06-12 11:48:40 -0300

    index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,14 @@
 import PageTitle from "UI/components/data-display/PageTitle/PageTitle";
 import SafeEnvironment from "UI/components/feedback/SafeEnvironment/SafeEnvironment";
 import UserInformation from "UI/components/data-display/UserInformation/UserInformation";
+import TextFieldMask from "UI/components/inputs/TextFieldMask/TextFieldMask";
+import { Button, Typography, Container } from "@material-ui/core";
+import {
+  FormElementsContainer,
+  ProfissionaisPaper,
+  ProfissionaisContainer,
+} from "UI/styles/pages/index.style";
+
 export default function Home() {
   return (
     <div>
@@ -11,12 +19,95 @@ export default function Home() {
           "Preencha seu endereço e veja todos os profissionais da sua localidade"
         }
       />
-      <UserInformation
-        picture={"Https://github.com/finotellicarlos.png"}
-        name={"Carlos"}
-        rating={3}
-        description={"São Paulo"}
-      />
+      <Container>
+        <FormElementsContainer>
+          <TextFieldMask
+            mask={"99.999-999"}
+            variant={"outlined"}
+            label={"Digite seu CEP"}
+            fullWidth
+          />
+          <Typography color={"error"}>CEP Inválido</Typography>
+          <Button
+            variant={"contained"}
+            color={"secondary"}
+            sx={{ width: "220px" }}
+          >
+            Buscar
+          </Button>
+        </FormElementsContainer>
+
+        <ProfissionaisPaper>
+          <ProfissionaisContainer>
+            <UserInformation
+              picture={"Https://github.com/finotellicarlos.png"}
+              name={"Carlos"}
+              rating={3}
+              description={"São Paulo"}
+            />
+            <UserInformation
+              picture={"Https://github.com/finotellicarlos.png"}
+              name={"Carlos"}
+              rating={3}
+              description={"São Paulo"}
+            />
+            <UserInformation
+              picture={"Https://github.com/finotellicarlos.png"}
+              name={"Carlos"}
+              rating={3}
+              description={"São Paulo"}
+            />
+            <UserInformation
+              picture={"Https://github.com/finotellicarlos.png"}
+              name={"Carlos"}
+              rating={3}
+              description={"São Paulo"}
+            />
+            <UserInformation
+              picture={"Https://github.com/finotellicarlos.png"}
+              name={"Carlos"}
+              rating={3}
+              description={"São Paulo"}
+            />
+            <UserInformation
+              picture={"Https://github.com/finotellicarlos.png"}
+              name={"Carlos"}
+              rating={3}
+              description={"São Paulo"}
+            />
+            <UserInformation
+              picture={"Https://github.com/finotellicarlos.png"}
+              name={"Carlos"}
+              rating={3}
+              description={"São Paulo"}
+            />
+            <UserInformation
+              picture={"Https://github.com/finotellicarlos.png"}
+              name={"Carlos"}
+              rating={3}
+              description={"São Paulo"}
+            />
+            <UserInformation
+              picture={"Https://github.com/finotellicarlos.png"}
+              name={"Carlos"}
+              rating={3}
+              description={"São Paulo"}
+            />
+            <UserInformation
+              picture={"Https://github.com/finotellicarlos.png"}
+              name={"Carlos"}
+              rating={3}
+              description={"São Paulo"}
+            />
+            <UserInformation
+              picture={"Https://github.com/finotellicarlos.png"}
+              name={"Carlos"}
+              rating={3}
+              description={"São Paulo"}
+            />
+          </ProfissionaisContainer>
+        </ProfissionaisPaper>
+      </Container>
     </div>
   );
 }
